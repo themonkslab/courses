114edd5fd79d3c2657a066ac89289edd4d93577e
Mauro Di Bert <maurodibert@gmail.com>
2022-11-30 08:02:55 -0300

    5.8 Implementando loop, 5.9 implementando logica juego

--- dart/5.app_linea_comando/5.5.1_rock_paper_scissors.dart
+++ dart/5.app_linea_comando/5.5.1_rock_paper_scissors.dart
@@ -1,7 +1,47 @@
 import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scissors } // Noten que no ponemos _trailing comma_
+// ya que es más fácil leerlo en una línea
 
 void main() {
-  stdout.write('Rock, paper or scissors? (r/p/s): ');
-  final userInput = stdin.readLineSync();
-  print(userInput);
+  final rng = Random();
+
+  while (true) {
+    print('-----------------------------------');
+    stdout.write('Rock, paper or scissors? (r/p/s): ');
+    final userInput = stdin.readLineSync();
+    if (userInput == 'r' || userInput == 'p' || userInput == 's') {
+      var playerMove;
+      if (userInput == 'r') {
+        playerMove = Move.rock;
+      } else if (userInput == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+      final randomNumber = rng.nextInt(3);
+      final computerMove = Move.values[randomNumber];
+      print('You played: $playerMove');
+      print('Computed played: $computerMove');
+      print('-----------------------------------');
+      if (playerMove == computerMove) {
+        print('It\'s a draw 😄');
+      } else if (playerMove == Move.rock && computerMove == Move.scissors ||
+          playerMove == Move.paper && computerMove == Move.rock ||
+          playerMove == Move.scissors && computerMove == Move.paper) {
+        print('You win!!! 🥳');
+      } else {
+        print('You lose 😭');
+      }
+    } else if (userInput == 'q') {
+      print('-----------------------------------');
+      print('I hope you enjoyed the game! 😃');
+      print('-----------------------------------');
+      break;
+    } else {
+      print('-----------------------------------');
+      print('Invalid input! 😳');
+    }
+  }
 }
