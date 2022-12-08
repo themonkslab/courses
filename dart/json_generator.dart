31ca697dde720586493dbbe2283f299bc7b70161
Mauro Di Bert <maurodibert@gmail.com>
2022-12-08 06:51:23 -0300

    json_generator wip1

---
+++ dart/json_generator.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
+}
+
+// 'contentUrl':
+//     'https://raw.githubusercontent.com/themonkslab/courses/main/dart/1.introduccion/1.1_sacar_provecho_del_curso.md',
+void main() async {
+  // Save directories as Directory in List, without 'assets'
+  final directories = <Directory>[];
+  final dir = Directory('./dart');
+
+  final List<FileSystemEntity> entities = await dir.list().toList();
+  for (var entity in entities) {
+    if (await FileSystemEntity.isDirectory(entity.path)) {
+      if (!(entity.path.substring(entity.path.lastIndexOf('/') + 1) ==
+          'assets')) {
+        directories.add(entity as Directory);
+      }
+    }
+  }
+
+  // Saves '.md' files in directory
+  final directoriesList = [];
+  for (var directory in directories) {
+    final articles = [];
+    final directoryNameWithUnderscore =
+        (directory.path.replaceAll('./dart/', ''));
+    final directoryName =
+        (directory.path.replaceAll('./dart/', '')).replaceAll('_', ' ');
+
+    final List<FileSystemEntity> subEntities = await directory.list().toList();
+    for (var entity in subEntities) {
+      if (await FileSystemEntity.isFile(entity.path)) {
+        final String articleExtension;
+        try {
+          articleExtension = "." + entity.uri.toString().split('.').last;
+        } catch (e) {
+          throw e;
+        }
+        if (articleExtension == '.md') {
+          final articleName = (entity.path
+              .replaceAll('./dart/$directoryNameWithUnderscore/', '')
+              .replaceAll('_', ' '));
+
+          final baseContentUrl =
+              'https://raw.githubusercontent.com/themonkslab/courses/main';
+
+          final contentUrl = entity.path.replaceFirst('.', baseContentUrl);
+
+          articles.add({
+            'path': entity.path,
+            'title': articleName,
+            'description': '',
+            // example: https://raw.githubusercontent.com/themonkslab/courses/main/dart/1.introduccion/1.2_que_esperamos_nosotros.md
+            'contentUrl': contentUrl,
+            'author': 'Mau Di Bert',
+            'published': '',
+          });
+          // mdFiles.add(entity as File);
+        }
+      }
+    }
+
+    directoriesList.add({
+      'path': directory.path,
+      'title': directoryName,
+      'description': '',
+      'articles': articles,
+    });
+
+    // directoriesMap[directory.path] = {
+    //   'articles': mdFiles,
+    // };
+  }
+
+  // final orderedDirectoriesMap = Map.fromEntries(directoriesMap.entries.toList()
+  //   ..sort(((a, b) => a.key.compareTo(b.key))));
+
+  // for (var orderedDirectory in orderedDirectoriesMap.entries) {}
+
+  // final orderedArticles = List.from(mdFiles.asMap().entries.toList()
+  //   ..sort(((a, b) => a.key.compareTo(b.key))));
+
+  // final Map sections = {};
+
+  // for (Directory directory in directories) {
+  //   final name =
+  //       (directory.path.replaceAll('./dart/', '')).replaceAll('_', ' ');
+  //   sections[int.parse(name[0])] = {
+  //     'title': name,
+  //     'path': directory.path,
+  //   };
+  // }
+
+  // final orderedSections = Map.fromEntries(
+  //     sections.entries.toList()..sort((e1, e2) => e1.key.compareTo(e2.key)));
+  // return orderedSections;
+}
