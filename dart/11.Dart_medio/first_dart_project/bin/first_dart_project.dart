13cb5fa3e85ae85eb780124572b0282b0de9c33b
Mauro Di Bert <maurodibert@gmail.com>
2023-01-20 08:47:26 -0300

    debuggear un proyecto DONE

--- dart/11.Dart_medio/first_dart_project/bin/first_dart_project.dart
+++ dart/11.Dart_medio/first_dart_project/bin/first_dart_project.dart
@@ -1,8 +1,15 @@
 import 'package:first_dart_project/first_dart_project.dart'
     as first_dart_project;
 
+String getUserName() => 'Mau';
+
 void main(List<String> arguments) {
   print('Hello world: ${first_dart_project.calculate()}!');
+
+  final userName = getUserName();
+
+  print(userName);
+
   for (var argument in arguments) {
     print(argument);
   }
